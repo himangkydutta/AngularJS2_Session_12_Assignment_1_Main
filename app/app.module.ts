5109c8af54889af51d5df858d582e74aff18a63e
himangkydutta <himangkydutta08@gmail.com>
2017-08-03 22:12:01 +0530

    Add files via upload

---
+++ app/app.module.ts
@@ -0,0 +1,31 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router'
+
+import { AppComponent } from './app.component';
+import { AddMovieComponent } from './Component/add-movie.component';
+import { ListMovieComponent } from './Component/list-movie.component';
+import { SearchPipe } from "./Pipe/search.pipe";
+import { ApplicationRoutes } from './app.routes'
+import { CapitalisePipe } from "./Pipe/capitalise.pipe";
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AddMovieComponent,
+    ListMovieComponent,
+    SearchPipe,
+    CapitalisePipe
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    RouterModule.forRoot(ApplicationRoutes),
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
